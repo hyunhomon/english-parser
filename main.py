8167476864c6bb73c7160294c8e3f964a5e141cc
hyunhomon <hyunhomon@gmail.com>
2024-07-11 02:50:43 +0900

    feat: tkinter gui

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-from modules import NLPPipeline
+import tkinter as tk
 
-def main():
-    text = "This is an example sentence. Here is another sentence with different structure. And finally, the third sentence."
+from modules import NLPPipeline
+from tkinter import messagebox
+
+def nlp(text):
     pipeline = NLPPipeline()
     tokens, pos_tags, parse_tree = pipeline.process(text)
 
-    print("Tokens:", tokens)
-    print("POS Tags:", pos_tags)
-    print("Parse Tree:")
-    print(parse_tree)
+    return pos_tags
+
+def show_widget(root):
+    label = tk.Label(root, text='Enter some text:')
+    label.pack()
+    entry = tk.Entry(root)
+    entry.pack()
+    submit_button = tk.Button(root, text='Submit', command=lambda: show_result(entry))
+    submit_button.pack()
+
+def show_result(entry):
+    text = entry.get()
+    messagebox.showinfo('Result', f'pos_tags: { nlp(text) }')
+
+def main():
+    root = tk.Tk()
+    root.title('english-parser')
+    root.geometry("640x400+100+100")
+    root.resizable(False, False)
+
+    show_widget(root)
+
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
